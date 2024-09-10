7fb8fe7d7e8dfb25c5802c2e4b2ce159f26cf981
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2024-09-10 22:56:40 +0200

    Updated npm modules. Swapped body-parser for express builtin. Activated CORS in routes, added 404 path. Next: refactoring routes to seperate files

--- routes/app.mjs
+++ routes/app.mjs
@@ -1,14 +1,13 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
 const port = process.env.PORT || 1338; // export PORT=1338 in terminal to set preferred port number
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import documents from "./docs.mjs";
+import documents from "../docs.mjs";
 
 const app = express();
 
@@ -18,14 +17,17 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(process.cwd(), "public")));
 
+app.use(cors());
+
 // don't show the log when it is test
 if (process.env.NODE_ENV !== 'test') {
     // use morgan to log at command line
     app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.post("/", async (req, res) => {
     const result = await documents.addOne(req.body);
@@ -54,6 +56,28 @@ app.get('/new', (res) => {
     return res.render("doc");
 });
 
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500).json({
+        "errors": [
+            {
+                "status": err.status,
+                "title":  err.message,
+                "detail": err.message
+            }
+        ]
+    });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`API backend listening on port ${port}`)
 });
