92b22a0edc98059132e192d2e2f781335205be80
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2024-09-12 11:54:49 +0200

    Changed route return to JSON

--- routes/id.mjs
+++ routes/id.mjs
@@ -4,10 +4,7 @@ import express from 'express';
 var router = express.Router();
 
 router.get('/:id', async (req, res) => {
-    res.render(
-        "doc",
-        { doc: await documents.getOne(req.params.id) }
-    );
+    res.json({data: await documents.getOne(req.params.id)});
 });
 
 export default router;
--- routes/index.mjs
+++ routes/index.mjs
@@ -4,12 +4,12 @@ import express from 'express';
 var router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render("index", { docs: await documents.getAll() });
+    res.json({data: await documents.getAll() });
 });
 
 router.post("/", async (req, res) => {
     const result = await documents.addOne(req.body);
-    res.redirect(`/${result.lastID}`);
+    res.json(`/${result.lastID}`);
 });
 
 export default router;
