646b3821d5056ce9c0cae23855100223577af7da
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2024-09-17 22:47:57 +0200

    More function tests

--- test/integration-tests/route.test.js
+++ test/integration-tests/route.test.js
@@ -1,43 +1,60 @@
 process.env.NODE_ENV = 'test';
 
-const request = require('supertest');
-const app = require("../../app");
-let docId = "";
-
 describe('Reports', () => {
+    let app;
+    let request;
+
+    beforeAll(() => {
+        request = require('supertest');
+        app = require("../../app");
+        let docId = "";
+    });
+
+    afterAll(() => {
+        app.close();
+      });
+
     describe('GET /', () => {
         it('Should return all documents', async () => {
-            return request(app)
+            await request(app)
                 .get("/")
                 .expect('Content-Type', /json/)
                 .expect(200)
+                .then( ({ body }) => {
+                    expect(body).toHaveProperty("data");
+                });                
         });
     });
     // Should be part of a set up
     describe('POST /', () => {
         it('Should create a document', async () => {
-            return request(app)
+            await request(app)
             .post("/")
             .send({"title": "test", "content": "test new content"})
             .expect(201)
             .then(({ body }) => {
                 docId = body;
+                expect(body).toHaveLength(24);
             });
         });
     });
+
     describe('POST /update', () => {
         it('Should update a document', async () => {
-            return request(app)
+            await request(app)
             .post("/update")
             .send({"title": "Updated title", "content": "Updated content", 'id': docId})
-            .expect(200)
-        })
+            .expect(200);
+            const response = await request(app)
+            .get('/' + docId)
+            expect(JSON.parse(response.text).data[0]).toMatchObject({'_id': docId, "title": "Updated title", "content": "Updated content"})
+        });            
     });
 
     describe('GET /:id', () => {
         it('Should get a specific document', async () => {
-            return request(app)
-            .get('/:' + docId)
+            await request(app)
+            .get('/' + docId)
             .expect(200)
         });
     });
@@ -45,12 +62,10 @@ describe('Reports', () => {
     // Should be part of a teardown
     describe('POST /delete', () => {
         it('Should delete a document', async () => {
-            return request(app)
+            await request(app)
             .post('/delete')
             .send({"id": docId})
             .expect(410);
-        })
-    })
+        });
+    });
 });
-
-app.close();
