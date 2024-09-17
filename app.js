3341a286f76e0f46ec63f0b697affd3da410d8ff
Alf Pettersson <alpt22@student.bth.se>
2024-09-17 22:47:50 +0200

    Direct to port 1339 when NODE_ENV is test

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 require('dotenv/config');
 
-const port = process.env.PORT || 1338; // export PORT=1338 in terminal to set preferred port number
+let port;
+if (process.env.NODE_ENV === 'test') {
+    port = 1339;
+} else {
+    port = process.env.PORT || 1338; // export PORT=1338 in terminal to set preferred port number
+}
+
+// const port = process.env.PORT || 1338; // export PORT=1338 in terminal to set preferred port number
 
 // Import modules
 const express = require('express');
