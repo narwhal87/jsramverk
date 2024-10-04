79261f30d19f99a356ac6902b63298f28b1ab720
Alf Pettersson <alpt22@student.bth.se>
2024-10-04 13:23:06 +0200

    Add token verification support on backend

---
+++ verifyToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+    const token = req.header("auth-token");
+    if (!token) return res.status(400).send("Access denied")
+
+    try {
+        const verified = jwt.verify(token, process.env.SECRET_JWT);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.send(err);
+    }
+}
