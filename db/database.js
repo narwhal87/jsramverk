6e777484f8ef52bedf78c4d0551048edb12311c7
Alf Pettersson <alpt22@student.bth.se>
2024-10-04 12:25:55 +0200

    Add support for Mongoose db handler and Joi validation

--- db/database.js
+++ db/database.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
+require('dotenv').config(); // Give access to process.env variables from .env file
 const mongo = require("mongodb").MongoClient;
+const mongoose = require('mongoose');
 const collectionName = "documents";
+const collectionAuth = "users";
 
 const database = {
     getDb: async function getDb() {
@@ -22,7 +24,42 @@ const database = {
             collection: collection,
             client: client,
         };
+    },
+
+    getAuthDb: async function getAuthDb() {
+        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@jsramverk.8gn6u.mongodb.net/jsramverk?retryWrites=true&w=majority&appName=jsramverk`;
+
+        if (process.env.NODE_ENV === 'test') {
+            // eslint-disable-next-line max-len
+            dsn = `mongodb+srv://Tester:superTest@jsramverk.8gn6u.mongodb.net/test?retryWrites=true&w=majority&appName=jsramverk`;
+        }
+
+        const client = await mongo.connect(dsn);
+        const db = await client.db();
+        const collection = await db.collection(collectionAuth);
+
+        return {
+            collection: collection,
+            client: client,
+        };
+    },
+
+    mongoConnection: async function mongoConnection() {
+        try {
+            await mongoose.connect(process.env.MONGO_CONNECTION_URL); //Mongoose is itself an object, no return
+
+            //Check available collections
+            const collections = await (await mongoose.connection.db.listCollections().toArray()).map((coll) => {
+                return coll.name
+            })
+
+            console.log(collections)
+
+        } catch (err) {
+            handleError(error);
+        }
     }
+
 };
 
 module.exports = database;
---
+++ db/users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        // required: true,
+        // min: 1,
+        // max: 30
+    },
+    email: {
+        type: String,
+        // required: true,
+        // min: 5,
+        // max: 255
+    },
+    password: {
+        type: String,
+        // required: true,
+        // min: 1,
+        // max: 255
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
